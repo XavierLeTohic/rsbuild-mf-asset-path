cb3e011f4aec3c16bde698ab26f175ca3aba9a90
Xavier LE TOHIC <xavier.letohic@adevinta.com>
2024-06-04 18:10:32 +0200

    Add example with output.publicPath

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { ModuleFederationPlugin } from "@module-federation/enhanced/rspack";
 
 import { dependencies } from "./package.json";
 
@@ -8,37 +9,38 @@ export default defineConfig({
 	server: {
 		port: 5050,
 	},
-	dev: {
-		assetPrefix: "/",
-	},
-	output: {
-		assetPrefix: "/",
-	},
 	html: {
 		template: "./public/index.html",
 	},
-	moduleFederation: {
-		options: {
-			name: "host",
-			remotes: {},
-			exposes: {
-				"./mySharedHook": "./src/shared/hooks/mySharedHook.ts",
-			},
-			shared: [
-				{
-					react: {
-						requiredVersion: dependencies.react,
-						singleton: true,
-						eager: true,
+	tools: {
+		rspack: (config, { appendPlugins }) => {
+			// Will work in dev only if set to "/"
+			config.output.publicPath = "auto";
+
+			appendPlugins([
+				new ModuleFederationPlugin({
+					name: "host",
+					remotes: {},
+					exposes: {
+						"./mySharedHook": "./src/shared/hooks/mySharedHook.ts",
 					},
-					"react-dom": {
-						requiredVersion: dependencies["react-dom"],
-						singleton: true,
-						eager: true,
-					},
-				},
-				"./src/shared/hooks/mySharedHook.ts",
-			],
+					shared: [
+						{
+							react: {
+								requiredVersion: dependencies.react,
+								singleton: true,
+								eager: true,
+							},
+							"react-dom": {
+								requiredVersion: dependencies["react-dom"],
+								singleton: true,
+								eager: true,
+							},
+						},
+						"./src/shared/hooks/mySharedHook.ts",
+					],
+				}),
+			]);
 		},
 	},
 });
